488bd5b06e5189d0cf3eff24b55adbd7d2611665
uesleijf <uesleijf@gmail.com>
2020-04-22 14:03:27 -0300

    - testRetornaSomaIncorreta

--- tests/FunctionTeste.php
+++ tests/FunctionTeste.php
@@ -1,14 +1,20 @@
 <?php
 
+require 'functions.php';
 use PHPUnit\Framework\TestCase;
 
 class FunctionTest extends TestCase
 {
 	public function testRetornaSomaCorreta()
 	{
-		require 'functions.php';
-
 		$this->assertEquals(4, adicionar(2, 2));
+		$this->assertEquals(6, adicionar(2, 4));
+		$this->assertEquals(102, adicionar(100, 2));
+	}
+
+	public function testRetornaSomaIncorreta()
+	{
 		$this->assertNotEquals(4, adicionar(2, 3));
+		$this->assertNotEquals(4, adicionar(2, 1));
 	}
 }
