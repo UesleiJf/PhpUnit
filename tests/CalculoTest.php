e3c038db887636465b66993a5b64788c120b2e36
uesleijf <uesleijf@gmail.com>
2020-04-24 15:58:35 -0300

    - Comentários e divisão por zero

--- tests/CalculoTest.php
+++ tests/CalculoTest.php
@@ -8,38 +8,62 @@ class CalculoTest extends TestCase
 {
 	protected $calculo;
         
+    /**
+    * Setup
+    **/
     protected function setUp()
     {
         $this->calculo = new Calculo;
-        $this->calculo->num1 = 5;
-        $this->calculo->num2 = 0;
+        $this->calculo->num1 = 0;
+        $this->calculo->num2 = 5;
 
     }
     
+    /**
+    * @covers Calculo::soma
+    *
+    **/
 	public function testSoma()
 	{
 		$this->assertEquals(5, $this->calculo->soma());
 	}
 
+    /**
+    * @covers Calculo::subtracao
+    *
+    **/
     public function testSubtracao()
     {
-        $this->assertEquals(5, $this->calculo->subtracao());
+        $this->assertEquals(-5, $this->calculo->subtracao());
     }
 
+    /**
+    * @covers Calculo::multiplicacao
+    *
+    **/
     public function testMultiplicacao()
     {
         $this->assertEquals(0, $this->calculo->multiplicacao());
     }
-    
-    public function testDivisao()
-    {
+
+    /**
+    *
+    * @return boolean False e não vai para o teste de divisão caso 
+    * o $num2 seja = 0
+    *
+    **/
+    public function testDivisaoPorZero(){
+                
         if($this->calculo->num2 == 0){
             $this->assertEquals("Divisão por zero!", $this->calculo->divisao());
-        }else {
-            $this->assertEquals(2.5, $this->calculo->divisao());    
+        } else {
+            $this->assertEquals(0, $this->calculo->divisao());    
         }
     }
 
+    /**
+    * tearDown
+    **/
     protected function tearDown()
     {
         unset($this->calculo);
